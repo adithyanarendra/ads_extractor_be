c794f148309d89b256863c2ee6a2f42cd26697b6
adithya <adithyamnarendra@gmail.com>
2025-10-16 10:25:19 +0530

    conf updates

--- backend/gunicorn_conf.py
+++ backend/gunicorn_conf.py
@@ -2,6 +2,3 @@ workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
 bind = "0.0.0.0:8000"
 timeout = 120
-accesslog = "/var/www/backend/ads_extractor_be/backend/logs/gunicorn_access.log"
-errorlog = "/var/www/backend/ads_extractor_be/backend/logs/gunicorn_error.log"
-loglevel = "info"
