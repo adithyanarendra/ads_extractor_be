811eeb4d5aa8903eeb1445c55ad3201c87408756
adithya <adithyamnarendra@gmail.com>
2025-10-25 10:22:33 +0530

    db now vps local

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -6,38 +6,38 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-SUPABASE_DB_PASS_RAW = os.getenv("SUPABASE_DB_PASS")
-SUPABASE_DB_PASS = urllib.parse.quote_plus(SUPABASE_DB_PASS_RAW)
-DB_USER = "postgres.cehisscgqmcllzejlruz"
-DB_PORT = 5432
-DB_NAME = "postgres"
+# SUPABASE_DB_PASS_RAW = os.getenv("SUPABASE_DB_PASS")
+# SUPABASE_DB_PASS = urllib.parse.quote_plus(SUPABASE_DB_PASS_RAW)
+# DB_USER = "postgres.cehisscgqmcllzejlruz"
+# DB_PORT = 5432
+# DB_NAME = "postgres"
 # only for render deployment - only ipv4
 # DB_HOST = "aws-1-ap-southeast-1.pooler.supabase.com"
 # only for non-render deployment - ipv6 support
-DB_HOST = "db.cehisscgqmcllzejlruz.supabase.co"
+# DB_HOST = "db.cehisscgqmcllzejlruz.supabase.co"
 # only for non-render deployment - ipv6 support
-DATABASE_URL = (
-    f"postgresql+asyncpg://postgres:{SUPABASE_DB_PASS}@{DB_HOST}:5432/postgres"
-)
+# DATABASE_URL = (
+#     f"postgresql+asyncpg://postgres:{SUPABASE_DB_PASS}@{DB_HOST}:5432/postgres"
+# )
 # only for render deployment - only ipv4
 # DATABASE_URL = (
 #     f"postgresql+asyncpg://{DB_USER}:{SUPABASE_DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # )
 
-# LOCAL_DB_PASS_RAW = os.getenv("LOCAL_DB_PASS")
-# LOCAL_DB_PASS = urllib.parse.quote_plus(LOCAL_DB_PASS_RAW)
+LOCAL_DB_PASS_RAW = os.getenv("LOCAL_DB_PASS")
+LOCAL_DB_PASS = urllib.parse.quote_plus(LOCAL_DB_PASS_RAW)
 
 
 # # LOCAL DB HOST
-# DB_USER = os.getenv("LOCAL_DB_USER")
-# DB_HOST = os.getenv("LOCAL_DB_HOST")
-# DB_PORT = os.getenv("LOCAL_DB_PORT")
-# DB_NAME = os.getenv("LOCAL_DB_NAME")
+DB_USER = os.getenv("LOCAL_DB_USER")
+DB_HOST = os.getenv("LOCAL_DB_HOST")
+DB_PORT = os.getenv("LOCAL_DB_PORT")
+DB_NAME = os.getenv("LOCAL_DB_NAME")
 
 
-# DATABASE_URL = (
-#     f"postgresql+asyncpg://{DB_USER}:{LOCAL_DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# )
+DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{LOCAL_DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 
 engine = create_async_engine(
